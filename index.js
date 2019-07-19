581b3098bd7872ca10f3ee8493f7778f31ee96f4
Matthew Chen <mc.2green@gmail.com>
2019-07-19 14:58:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ function nestedTarget(){
    return document.getElementById("nested").querySelector("div.target");
 }
 
-function increaseRankBy(n){
-  const list = document.querySelectorAll('ul.ranked-list li');
-  var i = 0;
-  for (i = 0; i < list.length; i++){
-    list[i].innerHTML = parseInt(list[i].innterHTML) + n;
+function increaseRankBy(n) {
+  const list = document.quarySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < list.length; i++) {
+      list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+    }
   }
-}
 
 function deepestChild(){
   let currentNode = document.querySelector('#grand-node')
