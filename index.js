5c126b264f9bd2621a9f5054ac388030dbdc1fed
Matthew Chen <mc.2green@gmail.com>
2019-07-19 14:59:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector);
+}
 
+function nestedTarget(){
+   return document.getElementById("nested").querySelector("div.target");
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('ul.ranked-list li');
+  var i = 0;
+  for (i = 0; i < list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let currentNode = document.querySelector('#grand-node')
+  let nextNode = currentNode.children[0];
+  
+  while (nextNode) {
+    currentNode = nextNode;
+    nextNode = currentNode.children[0];
+  }
+  return (currentNode);
+
+}
